6f4b32bf7346a5daf9f524103df760c64bb42770
Augus-Fu <100711922+Augus-Fu@users.noreply.github.com>
2022-04-27 14:28:27 +0800

    Add files via upload

---
+++ 旋转函数/旋转函数.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<vector>
+#include<math.h>
+#include<numeric>//accumulate函数头文件
+using namespace std;
+/*
+给定一个长度为 n 的整数数组 nums 。
+假设 arrk 是数组 nums 顺时针旋转 k 个位置后的数组，我们定义 nums 的 旋转函数  F 为：
+F(k) = 0 * arrk[0] + 1 * arrk[1] + ... + (n - 1) * arrk[n - 1]
+返回 F(0), F(1), ..., F(n-1)中的最大值 。
+测试用例：
+输入: nums = [4,3,2,6]
+输出: 26
+解释:
+F(0) = (0 * 4) + (1 * 3) + (2 * 2) + (3 * 6) = 0 + 3 + 4 + 18 = 25
+F(1) = (0 * 6) + (1 * 4) + (2 * 3) + (3 * 2) = 0 + 4 + 6 + 6 = 16
+F(2) = (0 * 2) + (1 * 6) + (2 * 4) + (3 * 3) = 0 + 6 + 8 + 9 = 23
+F(3) = (0 * 3) + (1 * 2) + (2 * 6) + (3 * 4) = 0 + 2 + 12 + 12 = 26
+所以 F(0), F(1), F(2), F(3) 中的最大值是 F(3) = 26
+*/
+int maxRotateFunction(vector<int>& nums) {
+    int f = 0, n = nums.size();
+    int numSum = accumulate(nums.begin(), nums.end(), 0);//累加求和
+    for (int i = 0; i < n; i++) {
+        f += i * nums[i];
+    }
+    int res = f;
+    for (int i = n - 1; i > 0; i--) {
+        f += numSum - n * nums[i];
+        res = max(res, f);
+    }
+    return res;
+}
+int main()
+{
+    vector<int> a = { 4,3,2,6 };
+    cout << maxRotateFunction(a) << endl;
+    system("pause");
+    return 0;
+}
